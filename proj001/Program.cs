3e3091c1125f24a84556bc9f73c987b16d423242
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-20 01:09:51 +0300

    InitCommit

---
+++ proj001/Program.cs
@@ -0,0 +1,50 @@
+﻿/*  **Задача 54**
+Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+int m, n;
+Console.WriteLine("Enter the m dimension of the array: ");
+int.TryParse(Console.ReadLine()!, out m);
+Console.WriteLine("Enter the n dimension of the array: ");
+int.TryParse(Console.ReadLine()!, out n);
+int[,] array = new int[m, n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]}  ");
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int c = array[i, j];
+        int c2 = j - 1;
+        while (c2 >= 0 && array[i, c2] > c)
+        {
+            array[i, c2 + 1] = array[i, c2];
+            array[i, c2] = c;
+            c2--;
+        }
+    }
+}
+ Console.WriteLine();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write($"{array[i, j]}  ");
+    }
+    Console.WriteLine();
+}
